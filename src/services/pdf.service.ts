caee08bc6270b0c2ea95cb4faa02aa76996198d6
rafaelarbz <rafaela.souza@artemis5.com.br>
2024-10-03 16:38:51 -0300

    fix import

--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -1,5 +1,5 @@
 import pdfMake from "pdfmake/build/pdfmake"
-import pdfFonts from "pdfmake/build/vfs_fonts"
+import * as pdfFonts from "pdfmake/build/vfs_fonts"
 import { Content, TDocumentDefinitions } from "pdfmake/interfaces"
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
